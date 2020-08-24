75bb8de32c71f9f4fed136479868ade976fda2b0
michaelb <michael.bleuez2@gmail.com>
2020-08-24 14:20:48 +0200

    C bloc support in 13min ;-)

--- src/interpreters/C_original.rs
+++ src/interpreters/C_original.rs
@@ -1,44 +1,42 @@
 #[derive(Debug, Clone)]
 #[allow(non_camel_case_types)]
-pub struct Rust_original {
+pub struct C_original {
     support_level: SupportLevel,
     data: DataHolder,
     code: String,
-    rust_work_dir: String,
+    c_work_dir: String,
     bin_path: String,
     main_file_path: String,
+    compiler: String,
 }
 
-impl Interpreter for Rust_original {
-    fn new_with_level(data: DataHolder, support_level: SupportLevel) -> Box<Rust_original> {
-        let rwd = data.work_dir.clone() + "/rust_original";
+impl Interpreter for C_original {
+    fn new_with_level(data: DataHolder, support_level: SupportLevel) -> Box<C_original> {
+        let rwd = data.work_dir.clone() + "/c_original";
         let mut builder = DirBuilder::new();
         builder.recursive(true);
         builder
             .create(&rwd)
             .expect("Could not create directory for rust-original");
-        let mfp = rwd.clone() + "/main.rs";
-        let bp = String::from(&mfp[..mfp.len() - 3]); //so binary is named 'main'
-        Box::new(Rust_original {
+        let mfp = rwd.clone() + "/main.c";
+        let bp = String::from(&mfp[..mfp.len() - 2]);
+        Box::new(C_original {
             data,
             support_level,
             code: String::from(""),
-            rust_work_dir: rwd,
+            c_work_dir: rwd,
             bin_path: bp,
             main_file_path: mfp,
+            compiler: String::from("gcc"),
         })
     }
 
     fn get_supported_languages() -> Vec<String> {
-        vec![
-            String::from("rust"),
-            String::from("rust-lang"),
-            String::from("rs"),
-        ]
+        vec![String::from("c")]
     }
 
     fn get_name() -> String {
-        String::from("Rust_original")
+        String::from("C_original")
     }
 
     fn get_current_level(&self) -> SupportLevel {
@@ -73,7 +71,7 @@ impl Interpreter for Rust_original {
     }
 
     fn add_boilerplate(&mut self) -> Result<(), SniprunError> {
-        self.code = String::from("fn main() {") + &self.code + "}";
+        self.code = String::from("#include <stdio.h>\nint main() {") + &self.code + "return 0;}";
         Ok(())
     }
 
@@ -82,11 +80,10 @@ impl Interpreter for Rust_original {
         let mut _file =
             File::create(&self.main_file_path).expect("Failed to create file for rust-original");
         write(&self.main_file_path, &self.code).expect("Unable to write to file for rust-original");
-        let output = Command::new("rustc")
-            .arg("-O")
-            .arg("--out-dir")
-            .arg(&self.rust_work_dir)
+        let output = Command::new(&self.compiler)
             .arg(&self.main_file_path)
+            .arg("-o")
+            .arg(&self.bin_path)
             .output()
             .expect("Unable to start process");
 
--- src/interpreters/mod.rs
+++ src/interpreters/mod.rs
@@ -1,6 +1,7 @@
 include!("Python3_original.rs");
 include!("Rust_original.rs");
 include!("Bash_original.rs");
+include!("C_original.rs");
 include!("import.rs");
 #[macro_export]
     macro_rules! iter_types {
@@ -17,6 +18,11 @@ include!("import.rs");
                  )*
                 };{
             type Current = interpreters::Bash_original;
+                $(
+                    $code
+                 )*
+                };{
+            type Current = interpreters::C_original;
                 $(
                     $code
                  )*
