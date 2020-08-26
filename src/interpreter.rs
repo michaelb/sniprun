f5e8cdd0d86946d2201994be03ca9effa965c5e6
michaelb <michael.bleuez2@gmail.com>
2020-08-26 13:16:11 +0200

    more documentation

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -22,29 +22,51 @@ pub enum SupportLevel {
     Selected = 255,
 }
 
+///This is the trait all interpreters must implement.
+///The launcher run fucntions new() and run() from this trait.
 pub trait Interpreter {
     //create
     fn new(data: DataHolder) -> Box<Self> {
         Self::new_with_level(data, Self::get_max_support_level())
     }
+    /// This implies your interpreter struct should have a 'data' and a 'support_level' field.
+    /// I suggest you also add a 'code' String field to hold the code you want to modify and run
     fn new_with_level(data: DataHolder, level: SupportLevel) -> Box<Self>;
 
+    ///Return the (unique) name of your interpreter.
     fn get_name() -> String;
 
+    /// The languages (as filetype codes) supported by your interpreter; check ':set ft?' in neovim
+    /// on a file of your language if you are not sure
     fn get_supported_languages() -> Vec<String>;
+
     fn get_current_level(&self) -> SupportLevel;
     fn set_current_level(&mut self, level: SupportLevel);
+    fn get_data(&self) -> DataHolder;
+
+    /// You should override this method as soon as you wish to test your interpreter.
     fn get_max_support_level() -> SupportLevel {
         //to overwrite in trait impls
         return SupportLevel::Unsupported;
     }
-    fn get_data(&self) -> DataHolder;
 
+    /// This method should get the needed code from the data struct and eventually the files
+    /// of the project
     fn fetch_code(&mut self) -> Result<(), SniprunError>; //mut to allow modification of the current_level
+
+    /// This should add code that does not originate from the project to the code field in the
+    /// interpreter
     fn add_boilerplate(&mut self) -> Result<(), SniprunError>;
+
+    /// This should be used to build (compile) the code and produce an executable
+    /// this function should be left blank (return Ok(());) for interpreted languages.
     fn build(&mut self) -> Result<(), SniprunError>; //return path to executable
+
+    ///This should be used to execute a binary or execute the script
+    ///In case it's successfull, returns Ok( standart_output );
     fn execute(&mut self) -> Result<String, SniprunError>;
 
+    /// set the current support level to the one provided, run fetch(), add_boilerplate(), build() and execute() in order if each step is successfull
     fn run_at_level(&mut self, level: SupportLevel) -> Result<String, SniprunError> {
         self.set_current_level(level);
         self.fetch_code()
@@ -52,6 +74,7 @@ pub trait Interpreter {
             .and_then(|_| self.build())
             .and_then(|_| self.execute())
     }
+    /// default run function ran from the launcher (run_at_level(max_level))
     fn run(&mut self) -> Result<String, SniprunError> {
         self.run_at_level(self.get_current_level())
     }
--- src/interpreters/Rust_original.rs
+++ src/interpreters/Rust_original.rs
@@ -4,6 +4,8 @@ pub struct Rust_original {
     support_level: SupportLevel,
     data: DataHolder,
     code: String,
+
+    ///specific to rust
     rust_work_dir: String,
     bin_path: String,
     main_file_path: String,
@@ -11,14 +13,17 @@ pub struct Rust_original {
 
 impl Interpreter for Rust_original {
     fn new_with_level(data: DataHolder, support_level: SupportLevel) -> Box<Rust_original> {
+        //create a subfolder in the cache folder
         let rwd = data.work_dir.clone() + "/rust_original";
         let mut builder = DirBuilder::new();
         builder.recursive(true);
         builder
             .create(&rwd)
             .expect("Could not create directory for rust-original");
+
+        //pre-create string pointing to main file's and binary's path
         let mfp = rwd.clone() + "/main.rs";
-        let bp = String::from(&mfp[..mfp.len() - 3]); //so binary is named 'main'
+        let bp = String::from(&mfp[..mfp.len() - 3]); // remove extension so binary is named 'main'
         Box::new(Rust_original {
             data,
             support_level,
@@ -57,14 +62,18 @@ impl Interpreter for Rust_original {
     }
 
     fn fetch_code(&mut self) -> Result<(), SniprunError> {
+        //add code from data to self.code
         if !self
             .data
             .current_bloc
             .replace(&[' ', '\t', '\n', '\r'][..], "")
             .is_empty()
+            && self.support_level >= SupportLevel::Bloc
         {
             self.code = self.data.current_bloc.clone();
-        } else if !self.data.current_line.replace(" ", "").is_empty() {
+        } else if !self.data.current_line.replace(" ", "").is_empty()
+            && self.support_level >= SupportLevel::Line
+        {
             self.code = self.data.current_line.clone();
         } else {
             self.code = String::from("");
@@ -82,6 +91,8 @@ impl Interpreter for Rust_original {
         let mut _file =
             File::create(&self.main_file_path).expect("Failed to create file for rust-original");
         write(&self.main_file_path, &self.code).expect("Unable to write to file for rust-original");
+
+        //compile it (to the bin_path that arleady points to the rigth path)
         let output = Command::new("rustc")
             .arg("-O")
             .arg("--out-dir")
@@ -99,6 +110,7 @@ impl Interpreter for Rust_original {
     }
 
     fn execute(&mut self) -> Result<String, SniprunError> {
+        //run th binary and get the std output (or stderr)
         let output = Command::new(&self.bin_path)
             .output()
             .expect("Unable to start process");
