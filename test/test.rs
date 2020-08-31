809211f01295038ebbeb4aa659e618ba8eae63ea
michaelb <michael.bleuez2@gmail.com>
2020-08-31 20:32:02 +0200

    demo rust

--- test/test.rs
+++ test/test.rs
@@ -1,18 +1,26 @@
-fn main() {
-    let a = 4;
-    let b = 5;
-    println!("sum : {}", a + b);
+// will try to finish that later
+fn get_infinite_two() {
 
-    let a = 8 * 0 * 4 * 4;
-    let b = 10;
-    println!("div : {}", b / a);
+
+
+
+
+fn main() {
+   let stuff =
 
     fn have_two() -> u16 {
         return 2;
     }
-    let i = std::iter::once(have_two() * 2)
+
+
+
+    let i = std::iter::once(have_two())
         .map(|u| u * u)
         .next()
         .unwrap();
-    println!("hello n° {}", i + 0);
-}
+    println!("hello n° {}", i );
+
+
+    let stuff = 0;
+    let a = 7/stuff;
+    assert!(a > 0);
