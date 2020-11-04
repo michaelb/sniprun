b3f9910ff5cbaa65ed0cb4a8f67dc81f163d4aab
michaelb <michael.bleuez2@gmail.com>
2020-11-04 23:31:00 +0100

    good download release system :-)

--- install.sh
+++ install.sh
@@ -2,40 +2,75 @@
 
 set -o errexit
 
-version="v0.2.0"
+# version="v0.2.0"
+
+local_version=vv$(cat Cargo.toml | grep version | cut -d "\"" -f 2)
 name="sniprun"
+force_build=$1
 
 cargo_build() {
   if command -v cargo >/dev/null; then
-    echo "Trying to build Sniprun locally using Cargo."
     echo "Building..."
-    cargo build --release 2>/dev/null
+    cargo build --release &>/dev/null
     echo "Done"
   else
     echo "Could not find cargo in \$PATH: the Rust toolchain is required to build Sniprun"
     return 1
   fi
 }
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/michaelb/sniprun/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/' # Pluck JSON value
+}
 
 download() {
-  command -v curl >/dev/null &&
-    curl --fail --location "$1" --output target/release/sniprun
+  # command -v curl >/dev/null &&
+  # curl --fail --location "$1" --output target/release/sniprun
+  rm -rf download_dir
+  mkdir -p download_dir
+  cd download_dir
+  curl -s https://api.github.com/repos/michaelb/sniprun/releases/latest | grep "sniprun" | cut -d ":" -f 2,3 | tr -d \" | wget -qi -
+  mv -f sniprun ../target/release/
+  cd ..
 }
 
 fetch_prebuilt_binary() {
   echo "Downloading binary.."
-  url="not provided yet"
-  echo $url
   mkdir -p target/release
 
   if (download "$url"); then
     chmod a+x target/release/sniprun
+    echo "Done"
     return
   else
-    cargo_build || echo "Prebuilt binaries are not available for this platform."
+    return 1
   fi
 }
 
 arch=$(uname)
-echo "No pre-built binary available yet for platform: ${arch}."
-cargo_build
+if [ $arch != "Linux" ]; then
+  echo "Warning, sniprun needs Linux to work properly! Any behaviour from this point is not tested"
+fi
+
+remote_version=v$(get_latest_release)
+
+if [ $force_build ]; then
+  echo "Always compiling the latest commit (most bleeding-edge option)"
+  cargo_build
+else
+  #check if release is up to date
+  if [ $local_version == $remote_version ]; then
+    echo "Trying to get a up-to-date precompiled binary"
+    fetch_prebuilt_binary
+    success=$?
+  else
+    echo "Release version is not up to date, building from source"
+    cargo_build
+    success=$?
+  fi
+
+  # if nothing succeeded
+  if [ success == 1 ]; then
+    echo "Could not build (missing rust/cargo toolchain?). Getting an out-of-date release if available"
+    fetch_prebuilt_binary # get an older release
+  fi
+fi
