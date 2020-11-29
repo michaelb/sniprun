ae0b3aa42f7dc3811fe7e55f92049db1f7981788
michaelb <michael.bleuez2@gmail.com>
2020-11-29 18:16:26 +0100

    cleaning test + doc python

--- test/unindent.rs
+++
@@ -1,7 +0,0 @@
-use unindent::unindent;
-
-fn main() {
-    let string_literal = "\n    count = 0 \n    for i in range(1,40000000):\n        count += 1\n    print(count, end=\"\")";
-    println!("before:\n{}\n\n", string_literal);
-    println!("after: \n{}", unindent(string_literal));
-}
