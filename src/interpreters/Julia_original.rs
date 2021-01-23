ed2054686cce07170e56c4a7a43169ad26f209d7
Michael <Bleuez>
2021-01-23 11:43:03 +0100

    Julia interpreter

---
+++ src/interpreters/Julia_original.rs
@@ -0,0 +1,117 @@
+//Interpreter:| Julia_original      | julia       |
+//############|_____________________|_____________|________________<- delimiters to help formatting,
+//############| Interpretername     | language    | comment
+// Keep (but modify the first line after the :) if you wish to have this interpreter listed via SnipList
+#[derive(Clone)]
+#[allow(non_camel_case_types)]
+pub struct Julia_original {
+    support_level: SupportLevel,
+    data: DataHolder,
+    code: String,
+    imports: String,
+    main_file_path: String,
+    plugin_root: String,
+    cache_dir: String,
+}
+
+impl ReplLikeInterpreter for Julia_original {}
+
+impl Interpreter for Julia_original {
+    fn new_with_level(data: DataHolder, level: SupportLevel) -> Box<Julia_original> {
+        //create a subfolder in the cache folder
+        let rwd = data.work_dir.clone() + "/julia_original";
+        let mut builder = DirBuilder::new();
+        builder.recursive(true);
+        builder
+            .create(&rwd)
+            .expect("Could not create directory for julia-original");
+
+        //pre-create string pointing to main file's and binary's path
+        let mfp = rwd.clone() + "/main.jl";
+
+        let pgr = data.sniprun_root_dir.clone();
+        Box::new(Julia_original {
+            data,
+            support_level: level,
+            code: String::from(""),
+            imports: String::from(""),
+            main_file_path: mfp,
+            plugin_root: pgr,
+            cache_dir: rwd,
+        })
+    }
+
+    fn get_name() -> String {
+        String::from("Julia_original")
+    }
+
+    fn behave_repl_like_default() -> bool {
+        false
+    }
+
+    fn get_supported_languages() -> Vec<String> {
+        vec![String::from("julia"), String::from("jl")]
+    }
+
+    fn get_current_level(&self) -> SupportLevel {
+        self.support_level
+    }
+    fn set_current_level(&mut self, level: SupportLevel) {
+        self.support_level = level;
+    }
+
+    fn get_data(&self) -> DataHolder {
+        self.data.clone()
+    }
+
+    fn get_max_support_level() -> SupportLevel {
+        SupportLevel::Bloc
+    }
+
+    fn fetch_code(&mut self) -> Result<(), SniprunError> {
+        if !self
+            .data
+            .current_bloc
+            .replace(&[' ', '\t', '\n', '\r'][..], "")
+            .is_empty()
+            && self.get_current_level() >= SupportLevel::Bloc
+        {
+            self.code = self.data.current_bloc.clone();
+        } else if !self.data.current_line.replace(" ", "").is_empty()
+            && self.get_current_level() >= SupportLevel::Line
+        {
+            self.code = self.data.current_line.clone();
+        } else {
+            self.code = String::from("");
+        }
+
+        Ok(())
+    }
+    fn add_boilerplate(&mut self) -> Result<(), SniprunError> {
+        Ok(())
+    }
+    fn build(&mut self) -> Result<(), SniprunError> {
+        // info!("python code:\n {}", self.code);
+        write(&self.main_file_path, &self.code)
+            .expect("Unable to write to file for julia_original");
+        Ok(())
+    }
+    fn execute(&mut self) -> Result<String, SniprunError> {
+        let output = Command::new("julia")
+            .arg(&self.main_file_path)
+            .output()
+            .expect("Unable to start process");
+        if output.status.success() {
+            return Ok(String::from_utf8(output.stdout).unwrap());
+        } else {
+            return Err(SniprunError::RuntimeError(
+                String::from_utf8(output.stderr.clone())
+                    .unwrap()
+                    .lines()
+                    .last()
+                    .unwrap_or(&String::from_utf8(output.stderr).unwrap())
+                    .to_owned(),
+            ));
+        }
+    }
+}
