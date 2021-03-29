52386a33ff8cb81f6ab0da7155881f72e5b81595
Michael Bleuez <michael.bleuez2@gmail.com>
2021-03-29 21:59:26 +0200

    test for include in C

--- src/interpreters/C_original.rs
+++ src/interpreters/C_original.rs
@@ -174,6 +174,11 @@ mod test_c_original {
     use super::*;
 
     #[test]
+    fn run_all() {
+        simple_print();
+        test_include();
+    }
+
     fn simple_print() {
         let mut data = DataHolder::new();
         data.current_bloc = String::from("printf(\"1=1\\n\");");
@@ -183,5 +188,23 @@ mod test_c_original {
         // should panic if not an Ok()
         let string_result = res.unwrap();
         assert_eq!(string_result, "1=1\n");
+    }
+
+    fn test_include() {
+        let mut data = DataHolder::new();
+        data.current_bloc = String::from("printf(\"%f\\n\", cos(M_PI));");
+
+        data.filepath = String::from("ressources/cccccc.c");
+        let mut file = File::create(&data.filepath).unwrap();
+        file.write_all(b"#include <math.h>").unwrap();
+        
+
+
+        let mut interpreter = C_original::new(data);
+        let res = interpreter.run_at_level(SupportLevel::Import);
+
+        // should panic if not an Ok()
+        let string_result = res.unwrap();
+        assert_eq!(string_result, "-1.000000\n");
     }
 }
