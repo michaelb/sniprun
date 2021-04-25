58c1620b2018459021ad858f7f30c4f43bec2609
Michael Bleuez <michael.bleuez2@gmail.com>
2021-04-25 12:24:53 +0200

    decent interpreter option getter

--- src/interpreters/import.rs
+++ src/interpreters/import.rs
@@ -2,7 +2,7 @@ use crate::error::SniprunError;
 use crate::interpreter::{Interpreter, InterpreterUtils, ReplLikeInterpreter, SupportLevel};
 use crate::DataHolder;
 use log::info;
-use serde_json::Value;
+use serde_json;
 
 use crate::interpreters;
 use crate::iter_types;
@@ -12,6 +12,7 @@ use std::io::prelude::*;
 use std::process::Command;
 
 use neovim_lib::NeovimApi;
+
 
 //indentation
 use unindent::unindent;
