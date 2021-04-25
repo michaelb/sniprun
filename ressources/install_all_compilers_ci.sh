5d98ee744caa84c316da97233d81654a1b80c617
Michael Bleuez <michael.bleuez2@gmail.com>
2021-04-25 11:36:12 +0200

    test github cache dependencies

---
+++ ressources/install_all_compilers_ci.sh
@@ -0,0 +1,13 @@
+sudo apt install haskell-platform -y
+sudo apt install -y nodejs npm 
+sudo npm install -g coffee-script 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9  
+sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/' 
+sudo apt install r-base 
+sudo apt install gnat 
+sudo apt install scala 
+pip3 install jupyter 
+sudo apt install lua5.3 
+./ressources/go_install.sh 
+export PATH=$PATH:$HOME/golang/go/bin/
+
