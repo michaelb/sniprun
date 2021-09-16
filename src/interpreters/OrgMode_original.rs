ab838ea9ecf3150f289ce3d6afd99436b4abc1d8
Michael Bleuez <michael.bleuez2@gmail.com>
2021-09-16 16:59:13 +0200

    orgmode patch, patch release

--- src/interpreters/OrgMode_original.rs
+++ src/interpreters/OrgMode_original.rs
@@ -17,7 +17,7 @@ impl OrgMode_original {
 
 
         //first check if we not on boundary of block
-        if self.data.current_line.starts_with("#+NAME") {
+        if self.data.current_line.trim_start().starts_with("#+NAME") || self.data.current_line.trim_start().starts_with("+name"){
             let next_line = real_nvim_instance
                 .get_current_buf()
                 .unwrap()
@@ -28,8 +28,8 @@ impl OrgMode_original {
         }
 
 
-        if self.data.current_line.starts_with("#+BEGIN_SRC") || self.data.current_line.starts_with("#+begin_src") {
-            let flavor = self.data.current_line.split_whitespace().nth(1).unwrap_or("").to_owned();
+        if self.data.current_line.trim_start().starts_with("#+BEGIN_SRC") || self.data.current_line.trim_start().starts_with("#+begin_src") {
+            let flavor = self.data.current_line.trim_start().split_whitespace().nth(1).unwrap_or("").to_owned();
             let end_line = real_nvim_instance
                 .get_current_buf()
                 .unwrap()
@@ -46,7 +46,7 @@ impl OrgMode_original {
                     .get_lines(&mut real_nvim_instance, i - 1, i, false)
                     .unwrap()
                     .join("");
-                if line_i.starts_with("#+END_SRC") || line_i.starts_with("#+end_src") {
+                if line_i.trim_start().starts_with("#+END_SRC") || line_i.trim_start().starts_with("#+end_src") {
                     //found end of bloc
                     self.data.current_bloc = code_bloc.join("\n");
                     info!("line to extract filetype from: {:?}", line_i.split_whitespace().collect::<Vec<_>>());
@@ -67,9 +67,8 @@ impl OrgMode_original {
                     .get_lines(&mut real_nvim_instance, i - 1, i, false)
                     .unwrap()
                     .join("");
-                if line_i.starts_with("#+BEGIN_SRC") {
-                    info!("line to extract filetype from: {:?}", line_i.split_whitespace().collect::<Vec<_>>());
-                    let flavor = line_i.split_whitespace().nth(1).unwrap_or("").to_owned();
+                if line_i.trim_start().starts_with("#+BEGIN_SRC") {
+                    let flavor = line_i.trim_start().split_whitespace().nth(1).unwrap_or("").to_owned();
                     return self.filetype_from_str(&flavor);
                 }
             }
@@ -77,7 +76,7 @@ impl OrgMode_original {
         String::new()
     }
 
-    /// Convert markdowncode block flavor to filetype
+    /// Convert orgmode code block flavor to filetype
     pub fn filetype_from_str(&self, s: &str) -> String {
         let cleaned_str = s.replace(&['{', '}', '.'][..], "");
         match cleaned_str.as_str() {
