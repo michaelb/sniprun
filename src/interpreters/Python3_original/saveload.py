1d96867454aa889d81ae38a3fbb580426cdccc97
Michael Bleuez <michael.bleuez2@gmail.com>
2022-01-09 15:41:42 +0100

    just in case but...

--- src/interpreters/Python3_original/saveload.py
+++ src/interpreters/Python3_original/saveload.py
@@ -1,21 +1,21 @@
 # function has a complicated name so it NEVER gets overriden by user's defs
-def sniprun142859_save(filename='memo', globals_=None):
+def sniprun142859_save(sniprun_filename='memo', globals_=None):
 
     globals_ = globals_ or globals()
     import klepto
-    dict_to_save = {}
+    sniprun_dict_to_save = {}
     for key, value in globals_.items():
         if not key.startswith('__'):
-            dict_to_save[key] = value
+            sniprun_dict_to_save[key] = value
             # print('added %s to save' % key)
-    a = klepto.archives.dir_archive(name=filename, dict=dict_to_save)
-    a.dump()
+    sniprun_a = klepto.archives.dir_archive(name=sniprun_filename, dict=sniprun_dict_to_save)
+    sniprun_a.dump()
 
 
-def sniprun142859_load(filename='memo', globals_=None):
+def sniprun142859_load(sniprun_filename='memo', globals_=None):
     import klepto
-    b = klepto.archives.dir_archive(filename)
-    for key in b.archive.keys():
-        b.load(key)
+    sniprun_b = klepto.archives.dir_archive(sniprun_filename)
+    for key in sniprun_b.archive.keys():
+        sniprun_b.load(key)
         # print("loading %s " % key)
-        globals()[key] = b[key]
+        globals()[key] = sniprun_b[key]
