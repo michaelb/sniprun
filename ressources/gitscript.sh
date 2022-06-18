f4779ffb91fb11903db47fbe821d5f8b569cfa5c
Michael Bleuez <michael.bleuez2@gmail.com>
2022-06-18 12:18:57 +0200

    show commit in dev SnipInfo

--- ressources/gitscript.sh
+++ ressources/gitscript.sh
@@ -8,7 +8,16 @@ get_latest_release() {
 local_version=v$(cat Cargo.toml | grep version | cut -d "\"" -f 2)
 remote_version=$(get_latest_release)
 
-echo -n "Version : " $local_version
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
+
+if [[ "$branch_name" == *"dev" ]]; then
+    commit=$(git rev-parse --short HEAD)
+    echo -n "Version : " $local_version "-" $commit
+else
+    # on main branch, only show version
+    echo -n "Version : " $local_version
+fi
+
 if [ $local_version == $remote_version ]; then
   echo " (up-to-date)"
 else
