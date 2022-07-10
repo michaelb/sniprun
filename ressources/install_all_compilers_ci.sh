f96e10adf2dec88858a33de6473dd57a1a019753
Michael Bleuez <michael.bleuez2@gmail.com>
2022-07-10 10:26:58 +0200

    no sudo + fix ubuntu version

--- ressources/install_all_compilers_ci.sh
+++ ressources/install_all_compilers_ci.sh
@@ -1,22 +1,22 @@
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt-get update
-sudo apt-get install neovim # install neovim 0.5+
+add-apt-repository ppa:neovim-ppa/stable -y
+apt-get update
+apt-get install neovim # install neovim 0.5+
 
-sudo apt install haskell-platform -y
-sudo apt install -y nodejs npm 
-sudo npm install -g coffee-script 
-sudo npm install -g typescript
-sudo npm install -g ts-node
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9  
-sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/' 
-sudo apt install r-base 
-sudo apt install gnat 
-sudo apt install scala 
+apt-get install haskell-platform -y
+apt-get install -y nodejs npm 
+npm install -g coffee-script 
+npm install -g typescript
+npm install -g ts-node
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9  
+add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu focal-cran40/' 
+apt-get install r-base 
+apt-get install gnat 
+apt-get install scala 
 pip3 install jupyter 
-sudo apt install lua5.3 
-sudo apt install sagemath
-sudo apt install gprolog
-sudo apt install dotnet
+apt-get install lua5.3 
+apt-get install sagemath
+apt-get install gprolog
+apt-get install dotnet
 ./ressources/go_install.sh 
 export PATH=$PATH:$HOME/golang/go/bin/
 
