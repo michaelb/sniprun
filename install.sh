dca8dd2f387d4731507b059e7f3c0690ea61b07f
Michael B <michael.bleuez2@gmail.com>
2023-10-04 13:38:24 +0200

    stricter sed in install.sh

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ cargo_build() {
 }
 
 get_latest_release() {
-  curl --silent "https://api.github.com/repos/michaelb/sniprun/releases/latest" | LC_ALL=C tr -d "\n" | sed -e 's|^.*\("tag_name".*\)|\1|' | cut -d'"' -f4 # Pluck JSON value
+  curl --silent "https://api.github.com/repos/michaelb/sniprun/releases/latest" | LC_ALL=C tr -d "\n" | sed -e 's|^.*\("tag_name"[^,]*\).*|\1|' | cut -d'"' -f4 # sed keeps matchgroup "tag_name": "vX.Z.Z", cut -d '"' does the rest
 }
 
 # download the sniprun binary (of the specified version) from Releases
