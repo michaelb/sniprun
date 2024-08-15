c5be472faaff935d315b741ad2dd10e011944557
Michael B <michael.bleuez2@gmail.com>
2024-08-15 10:12:26 +0200

    format

--- lua/sniprun/live_mode.lua
+++ lua/sniprun/live_mode.lua
@@ -13,21 +13,20 @@ function deepcopy(orig)
         copy = orig
     end
     return copy
-
 end
 
 function M.run()
-    local sa = require('sniprun.api')
-    local line = vim.api.nvim_win_get_cursor(0)[1]
-    local ft = vim.bo.filetype
-    local opts = deepcopy(require('sniprun').config_values)
-    opts.display  = opts.live_display
+    local sa            = require('sniprun.api')
+    local line          = vim.api.nvim_win_get_cursor(0)[1]
+    local ft            = vim.bo.filetype
+    local opts          = deepcopy(require('sniprun').config_values)
+    opts.display        = opts.live_display
     opts.show_no_output = {}
-    sa.run_range(line,line, ft, opts)
+    sa.run_range(line, line, ft, opts)
 end
 
 function M.enable()
-  vim.cmd [[
+    vim.cmd [[
     augroup _sniprunlive
      autocmd!
      autocmd TextChanged * lua require'sniprun.live_mode'.run()
@@ -35,30 +34,30 @@ function M.enable()
     augroup end
     lua require'sniprun.live_mode'.run()
   ]]
-  vim.notify "Enabled Sniprun live mode"
+    vim.notify "Enabled Sniprun live mode"
 end
 
 function M.disable()
-  M.remove_augroup "_sniprunlive"
-  require('sniprun.display').clear_virtual_text()
-  vim.notify "Disabled Sniprun live mode"
+    M.remove_augroup "_sniprunlive"
+    require('sniprun.display').clear_virtual_text()
+    vim.notify "Disabled Sniprun live mode"
 end
 
 function M.toggle()
-  if vim.fn.exists "#_sniprunlive#TextChanged" == 0 then
-    M.enable()
-  else
-    M.disable()
-  end
+    if vim.fn.exists "#_sniprunlive#TextChanged" == 0 then
+        M.enable()
+    else
+        M.disable()
+    end
 end
 
 function M.remove_augroup(name)
-  if vim.fn.exists("#" .. name) == 1 then
-    vim.cmd("au! " .. name)
-  end
+    if vim.fn.exists("#" .. name) == 1 then
+        vim.cmd("au! " .. name)
+    end
 end
 
 vim.cmd [[ command! SnipLive execute 'lua require("sniprun.live_mode").toggle()' ]]
-vim.api.nvim_set_keymap("n", "<Plug>SnipLive", ":lua require'sniprun.live_mode'.toggle()<CR>",{silent=true})
+vim.api.nvim_set_keymap("n", "<Plug>SnipLive", ":lua require'sniprun.live_mode'.toggle()<CR>", { silent = true })
 
 return M
