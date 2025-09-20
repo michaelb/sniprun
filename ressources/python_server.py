514d1c68216e59b22b59c1cec9e93c5a2e1dc241
Michael Bleuez <michael.bleuez2@gmail.com>
2025-09-20 11:32:33 +0200

    less verbose test http server

--- ressources/python_server.py
+++ ressources/python_server.py
@@ -17,7 +17,7 @@ class S(BaseHTTPRequestHandler):
         self.end_headers()
 
     def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        # logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         self._set_response()
         self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
 
